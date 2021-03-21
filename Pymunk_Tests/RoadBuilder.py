4257e77f9fafa417dbc2be4492d6b07f491f9c1d
Filbert-code <alexlingfungfilbert@gmail.com>
2021-03-20 20:59:26 -0700

    Working on the random terrain generator function in RoadBuilder

--- Pymunk_Tests/RoadBuilder.py
+++ Pymunk_Tests/RoadBuilder.py
@@ -1,4 +1,6 @@
 import pymunk as pm
+from random import randrange
+import math
 
 
 class RoadBuilder:
@@ -24,15 +26,28 @@ class RoadBuilder:
         self._space.add(*static_segments)
         return static_segments
 
-    def random_terrain_generator(self, vs, length):
+    def random_terrain_generator(self, vs, length, total_segments):
         """
         Randomly generates a road that builds towards the positive x-direction with
         each segment being a fixed length
+        :param total_segments: total number of segments to be made
         :param length: length of the road Segment
         :param vs: Starting vertex
         :return: The Body of road Segments
         """
         # Example: vs = (0, 0)
+        vertices = [vs]
+        x_deltas = []
+        y_deltas = []
+        # starting from 1 to be able to append new vertices to the vertices list that already contains 1
+
+        for seg_num in range(1, total_segments):
+            delta_y = randrange(-length, length)
+            delta_x = math.sqrt(length**2 - delta_y**2)
+            # append a new vertex using the coordinates of the previous vertex
+            prev_vertex = vertices[seg_num - 1]
+            new_vertex = prev_vertex[0] + delta_x, prev_vertex[1] + delta_y
+            vertices.append(new_vertex)
 
 
 
